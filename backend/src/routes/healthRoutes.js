cddec927ab2784609953e8c8d9b970bce568f150
Hegavasanthan44 <hegavasanth@gmail.com>
2026-09-15 16:51:15 +0530

    feat: add JWT authentication and role authorization

--- backend/src/routes/healthRoutes.js
+++ backend/src/routes/healthRoutes.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const protect = require("../middleware/authMiddleware");
+const authorize = require("../middleware/roleMiddleware");
 
 const router = express.Router();
 
@@ -6,6 +8,22 @@ router.get("/", (req, res) => {
   res.json({
     success: true,
     message: "API is healthy",
+  });
+});
+
+router.get("/protected", protect, (req, res) => {
+  res.json({
+    success: true,
+    message: "You are authenticated",
+    user: req.user,
+  });
+});
+
+router.get("/admin-only", protect, authorize("admin"), (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome Admin",
+    user: req.user,
   });
 });
 
